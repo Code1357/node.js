c71e8873fd005c2c4a5697f471e637a7ca684648
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-05-17 07:33:39 +0900

    simple_routes

---
+++ simple_routes/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const port = 3000;
+const http = require('http');
+const httpStatus = require('http-status-codes');
+
+// '経路': 'レスポンスの対応'  を定義するマップ
+const routeResponseMap = {
+  '/info': '<h1>Info Page/情報ページ</h1>',
+  '/contact': '<h1>Contact/お問い合わせ</h1>',
+  '/about': '<h1>Learn More About Us/私たちについて知っていただきたい事</h1>',
+  '/hello': '<h1>Say hello by emailing us <a href="mailto:mailing@gmail.com">here</a>/当方へのご連絡は、<a href="mailto:mailing@gmail.com">ここ</a>にメールを送信してださい</h1>',
+  '/error': '' // ここにerror.404ステータスコードでレスポンスを記述
+};　           // モジュールからステータスコードは　httpStatus.NOT_FOUND　
+　
+
+
+const app = http.createServer((req, res) => {　// req:リクエスト情報を受けている
+  res.writeHead(httpStatus.OK, {
+    'Content-Type': 'text/html; charset=utf-8'
+  });
+
+  //　リクエストの経路がマップで定義されているかチェック
+  if (routeResponseMap[req.url]) { // リクエストのURLが○○だったら
+    res.end(routeResponseMap[req.url]); // リクエストのURLに基づいたページを返す
+  } else {
+    // デフォルトのHTMLでレスポンス
+    res.end('<h1>Welcome!/ようこそ！</h1>'); // 該当なければ...を返す
+  }
+});
+
+// http://localhost:3000/
+app.listen(port);
+
+// サーバ状況をログに出力
+console.log(`サーバが起動してポート ${port} を監視しています`)
--- simple_server_03/main.js
+++ simple_server_03/main.js
@@ -6,37 +6,42 @@ const http = require('http'); // node.jsのコアモジュール
 const httpStatus = require('http-status-codes'); // 依存のモジュール
 const app = http.createServer(); // サーバーを作る
 
- // JavaScriptオブジェクトを文字列に変換する関数
+// JavaScriptオブジェクトを文字列に変換する関数
 const getJSONString = obj => {
   return JSON.stringify(obj, null, 2);
 }
 
-app.on('request',(req,res) => {
+app.on('request', (req, res) => {
   // リクエストを監視
-  let body = [] // データを入れる配列の箱を準備
-  req.on('data',(bodyData) => {
+  let body = [] // チャンクとして届くデータを順番に入れる箱（配列）を準備する
+
+  // 以下、POSTしたデータ
+  req.on('data', (bodyData /*引数：データを引き受ける場所*/ ) => {　// .on('data')のイベントを定義（データ読み込み時イベント）
     // そのデータを別のコールバック関数で処理
-    body.push(bodyData); // 受信したデーtをbody配列に入れる,push:配列の最後に要素をを追加
+    body.push(bodyData); // 受信したデータをbody配列に入れる,push:配列の最後に要素をを追加
   });
-  req.on('end',() => { // データ転送の完了時に実行するコード
-    body = Buffer.concat(body).toString(); // body配列を文字列テキストに変換
+
+  req.on('end', () => { // .on('end')：ストリームから読み込むデータが完了したらのイベント
+    body = Buffer.concat(body).toString(); // Buffer(バイナリデータ）のbody配列を文字列テキストに変換,concat(body):配列を繋ぐ,toString():指定されたオブジェクトを表す文字列を返す
+
     // リクエストの内容をコンソールにロギングする
     console.log(`Reqest Body Contents: ${body}`);
-});
+  });
 
-console.log(`Method: ${getJSONString(req.method)}`); 
-console.log(`URL: ${getJSONString(req.url)}`);
-console.log(`Headers: $getJSONString(req.headers)}`);
+  //getJSONStringを使って、流れたように記述されたデータを整えてるだけ
+  console.log(`Method: ${getJSONString(req.method)}`); // (req.method):HTTPリクエストのメソッドを出力
+  console.log(`URL: ${getJSONString(req.url)}`); // (req.url):HTTPリクエストのURLを出力
+  console.log(`Headers: ${getJSONString(req.headers)}`); // (req.headers):HTTPリクエストのhesder情報を出力
 
-res.writeHead(httpStatus.OK, {　//　writeHead():heder情報の書き出し
-  'Content-Type':'text/html'
-});
+  res.writeHead(httpStatus.OK, {　//　writeHead():heder情報の書き出し
+    'Content-Type': 'text/html; charset=utf-8'
+  });
 
-// write():bodyへの書き出し
+  // write():bodyへの書き出し
 
-// 「このメッセージが画面に現れます」
-let resMessage = '<h1>This will show on the screen.</h1>';
-res.end(resMessage);
+  // 「このメッセージが画面に現れます」
+  let resMessage = '<h1>クライアントの画面に表示させます</h1>';
+  res.end(resMessage);　//  最後は必ず.end()で閉める
 });
 
 // ここでは変数にprot3000を指定しているので、port3000を開いて処理を実行となる(処理の内容は、app関数)
