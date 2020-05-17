d6d35c8dc8c285f02433a9a38c407549a0e7d6a3
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-05-17 12:50:36 +0900

    second_simple_routes

--- simple_routes/main.js
+++ simple_routes/main.js
@@ -10,13 +10,13 @@ const routeResponseMap = {
   '/contact': '<h1>Contact/お問い合わせ</h1>',
   '/about': '<h1>Learn More About Us/私たちについて知っていただきたい事</h1>',
   '/hello': '<h1>Say hello by emailing us <a href="mailto:mailing@gmail.com">here</a>/当方へのご連絡は、<a href="mailto:mailing@gmail.com">ここ</a>にメールを送信してださい</h1>',
-  '/error': '' // ここにerror.404ステータスコードでレスポンスを記述
-};　           // モジュールからステータスコードは　httpStatus.NOT_FOUND　
+  '/error': '<h1>Soryy,thh page yuou looking for is not here/ごめんなさい。あなたが探しているページは、ここにはありません</h1>' // ここにerror.404ステータスコードでレスポンスを記述
+};　           
 　
 
 
 const app = http.createServer((req, res) => {　// req:リクエスト情報を受けている
-  res.writeHead(httpStatus.OK, {
+  res.writeHead(httpStatus.OK, httpStatus.NOT_FOUND, {
     'Content-Type': 'text/html; charset=utf-8'
   });
 
