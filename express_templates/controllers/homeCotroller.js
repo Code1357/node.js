20e71df28453b6d76ce8941685acf19470aafdff
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-05-28 07:21:20 +0900

    express_template3

--- express_templates/controllers/homeCotroller.js
+++ express_templates/controllers/homeCotroller.js
@@ -1,6 +1,18 @@
 'use strict';
 
-exports.responsWithName = (req, res) => {
+/* ① exports.responsWithName = (req, res) => {
   // カスタムEJSビューで応答する
   res.render('index'); //res.render：ビューにあるHTMLの文字列をクライアントに送信する
-}
+} */
+
+exports.responsWithName = (req, res) => {
+  // リクエストのパラメータをローカル変数に代入
+  let paramsName = req.params.myName;
+
+  console.log(req.params); // { myName: 'choko' }
+  console.log(paramsName); // choko
+
+  // 変数nameに値を代入して、indexのローカル変数に渡す
+  res.render('index', {name: paramsName });
+};
+// ('index', {name: paramsName })は、nameが変数,paramsNameが値。この場合、indexを表示させるとともに、index内で記述したnameに値を渡せる。
--- express_templates/main.js
+++ express_templates/main.js
@@ -5,16 +5,32 @@ const express = require('express');
 const app = express();
 const homeController = require('./controllers/homeCotroller');
 
+// express-ejs-layoutsの設定
+const layouts = require('express-ejs-layouts');
+app.use(layouts); // ミドルェア層である事を設定
+
 // Express.jsアプリケーションビューエンジンにejsを設定する（viewフォルダでejsを格納して使えるようにする)
 app.set('view engine', 'ejs');
 
 
+// パスはindexに指定すればindex,nameを指定すればnameを呼び出せる
+// という事はURLを自由に設定できる？？？？
+/* ① responsWithName: app.get('/name', homeController.responsWithName); */
 
-responsWithName: app.get('/index', homeController.responsWithName);
-
+/* ルート・パラメータ
+   name(既に指定されたkey,プロパティ)
+   :myName(イメージは値が入る箱) 
+   例)
+    ルートパス：'/name/:myName'
+    訪問者が、
+    リクエストURL：http://localhost:3000/name/chokoと入力したら
+    myNameの箱にchokoが入る
+    そのパラメータを取得するには、req.paramsを使う
+    req.params: {'name': 'choko'}となる
+*/
+responsWithName: app.get('/name/:myName', homeController.responsWithName);
 
 
 // デプロイした時にどちらのポートが適用となるか比較している,process.env：文字列でないプロパティを代入すると暗黙で文字列に変換される
-// app.set('port', process.env.PORT || 3000); 
-
+app.set('port', process.env.PORT || 3000); 
 app.listen(port, console.log(`prot${app.get('port')}が起動しています`));
