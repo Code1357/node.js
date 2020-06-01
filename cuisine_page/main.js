6cac2122c46ace33426dc9d8e997f7004c88423c
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-06-01 10:40:32 +0900

    mongooose_test_01

--- cuisine_page/main.js
+++ cuisine_page/main.js
@@ -1,42 +1,29 @@
 'use strict';
+/*
+mongoose(パッケージ)を使った操作
+// 参考：
+https://mongoosejs.com
+https://mongoosejs.com/docs/connections.html
+https://mongoosejs.com/docs/schematypes.html
+*/
+// mongooseモジュールを読み込み
+const mongoose = require('mongoose');
+// データベースへの接続を設定
+mongoose.connect('mongodb://localhost:27017/recipe_db',
+{useNewUrlParser: true, useUnifiedTopology: true}
+);
+// データベースをdb変数に代入
+const db = mongoose.connection;
+// mongooseでMongoDBへ接続
+db.once('open', () => {
+  console.log('mongooseを使ってMongoDBに接続できました！')
+});
 
-// MongoDBモジュール,MongoClient:新しいインスタンスを作成
-const MongoClient = require('mongodb').MongoClient;
-// Node.jsのモジュール,error発生の確認するため
-const assert = require('assert').strict; // Node.jsの厳密モード
-// DBの接続先URL設定
-const dbUrl = 'mongodb://localhost:27017';
-// DBの名前
-const dbName = 'recipe_db';
-// MongoClientの新規作成
-const client = new MongoClient(dbUrl);
-
-// サーバ接続
-client.connect((error) => { // connect:指定のURLを使いMDBに接続
-  assert.strictEqual(null, error); // assert.strictEqual:パラメータの間の厳密な平等性をテスト実施
-  console.log('サーバーに接続できました');
-  const db = client.db(dbName);
-
-  // 新しいデータを追加
-  db.collection('contacts').insert({
-    name: 'ちょこ',
-    email: 'choko_test@gmail.com'
-  }, (error, db) => {
-    if (error) throw error;
-    console.log(db);
-  });
-
-  // データ(レコード)の取得
-  // // find(),jsの関数と違う。MongoDBでは一致が得られない時に空の配列を返す
-  db.collection('contacts').find().toArray((error, document) => {
-    assert.strictEqual(null, error);
-    console.log(document);
-  });
-
-  // 接続解除
-   client.close(() => { // close(callback):接続を閉じ,オプションのコールバックを呼び出す
-    console.log('接続終了');
-  });
+// mongoose.Schemaを使って新しいスキーマを作る,スキーマ:クラスのようなイメージで設計図となる部分
+const subscriberSchema = mongoose.Schema({
+  name: String, // プロパティ: データ型
+  email: String,
+  zipCode: Number
 });
 
 /*--------------------------------------------*/
