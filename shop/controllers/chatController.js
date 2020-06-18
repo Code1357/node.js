382854a64f545124584a7be369629074fbeae7d6
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-06-18 16:29:07 +0900

    shop_33

--- shop/controllers/chatController.js
+++ shop/controllers/chatController.js
@@ -1,20 +1,45 @@
 // ここからチャットのsocket接続を管理する
 'use strict';
 
+const Message = require('../models/message');
 
 // ソケットを接続するための処理
 module.exports = io => {
   io.on('connection', client => { // 新規ユーザーの接続を監視, connction(node.js)イベント,新しいTCPストリームが確立された時に発生する,クライアントがソケットチャンネルに接続した事を示す。
-console.log('新規接続');  
+    console.log('新規接続');
 
-client.on('disconnect', () => { // ユーザーの切断を監視,disconnect(node.js)イベント,切断のイベント
-  console.log('ユーザーは切断しました');
-});
+    // DBに保存されているデータを制限をつけてロードする
+    Message.find({}) // このスキーマの中を探し出す
+    .sort({createdAt: -1 }) // sort(並べ方を決める),createdAt(日付順),-1(降順)
+    .limit(10) // 最大数は10
+    .then(messages => {
+      client.emit("load all messages", messages.reverse()); // load all messages(カスタムイベント),messageの配列を逆にする.revers(js),自作のイベント
+    });
 
-client.on("message", () => { // 作成されるメッセージのイベントを監視, messeage(node.js)イベント,もしmessage受信したら
-  io.emit("message", { // emitメソッドで任意のテキストを接続している全部のクライアントに送信する
-    content: "Hello" // content,recipeApp.jsで設定
+    client.on('disconnect', () => { // ユーザーの切断を監視,disconnect(node.js)イベント,切断のイベント
+      console.log('ユーザーは切断しました');
+    });
+
+    // 決まったメッセージではなく、データを送信するように変更
+    client.on("message", data => { // データをmessageイベントの内容として送信
+      let messageAttributes = { // 受け取ったデータを全て集める
+        content: data.content,
+        userName: data.userName,
+        user: data.userId
+      };
+      let m = new Message(messageAttributes); // 設計したスキーマに該当一致するtypeの内容で
+      m.save() // 変数mを保存
+      .then(() => { // 保存に成功したら値を送信
+        io.emit("message", messageAttributes);
+      })
+      .catch(error => console.log(`chat Error:${error.message}`));
+    });
+
+
+    /* client.on("message", () => { // 作成されるメッセージのイベントを監視, messeage(node.js)イベント,もしmessage受信したら
+      io.emit("message", { // emitメソッドで任意のテキストを接続している全部のクライアントに送信する
+        content: "Hello" // content,recipeApp.jsで設定
+      });
+    }); */
   });
-});
-});
 };
--- shop/public/js/recipeApp.js
+++ shop/public/js/recipeApp.js
@@ -1,24 +1,66 @@
 'use strict';
 
-// jQeryで記述されている
+// jQeryで記述されている,/クライアント側の処理
 
 $(document).ready(() => {
-const socket = io(); // クライアントサイドでsocket.ioを初期化
+  const socket = io();
+  $("#chatForm").submit(() => { // フォーム全体の内容を送信
+    let text = $("#chat-input").val(); // 入力フィールドからテキストを取り込む
+    let userName = $("#chat-user-name").val();
+    let userId = $("#chat-user-id").val(); // 隠しフィールドのデータを取り込む
 
-// フォームが送出された時にイベントを発行
-$("#chatForm").submit(() => {
-  socket.emit("message");
-  $("#chat-input").val("");
-  return false;
-});
-// イベントを監視し、チャットボックスに記入
-socket.on("message", message => {
-  displayMessage(message.content); // contentプロパティは、chatController.jsのcontentで設定
-});
-// サーバー川から受信したメッセージをチャットボックスに表示
-let displayMessage = message => {
-  $("#chat").prepend($("<li>").html(message));
-};
+    socket.emit("message", { // フォーム全体のデータをサーバーに送るイベント
+      content: text,
+      userName: userName,
+      userId: userId
+    });
+    $("#chat_input").val("");
+    return false;
+  });
+
+  socket.on("message", message => {
+    displayMessage(message);
+  });
+
+  // 自作のイベントを監視する
+  socket.on("load all messages", data => {
+    data.forEach(message => {
+      displayMessage(message);
+    });
+  });
+
+  let displayMessage = message => {
+    $("#chat").prepend( // prepend(要素やテキストを追加する(js))
+      $("<li>").html(`
+				<strong class="message ${getCurrentUserClass(message.user)}">
+					${message.userName}
+				</strong>: ${message.content}
+			`) // ${message.userName}が実際にチャットに表示される名前,${message.content}が実際にチャットに表示されるコメント
+    );
+  };
+
+  let getCurrentUserClass = (id) => {
+    let userId = $("chat-user-id").val();
+    return userId === id ? "current-user" : "";
+  };
+
+
+
+  /* const socket = io(); // クライアントサイドでsocket.ioを初期化
+  // フォームが送出された時にイベントを発行
+  $("#chatForm").submit(() => {
+    socket.emit("message");
+    $("#chat-input").val("");
+    return false;
+  });
+  // イベントを監視し、チャットボックスに記入
+  socket.on("message", message => {
+    displayMessage(message.content); // contentプロパティは、chatController.jsのcontentで設定
+  });
+  // サーバー川から受信したメッセージをチャットボックスに表示
+  let displayMessage = message => {
+    $("#chat").prepend($("<li>").html(message));
+  }; */
 
 
   $("#modal-button").click(() => {
@@ -69,18 +111,18 @@ let addJoinButtonListener = () => {
 /* $(document).ready(() => {
   $("#modal-button").click(() => {
     $(".modal-body").html("");
-    $.get("/api/courses", (results = {}) => { 
+    $.get("/api/courses", (results = {}) => {
       let data = results.data; // データを表現するローカル変数を準備
       if (!data || !data.courses) return; // データオブジェクトがコースの情報を含んでいるかチェック
       data.courses.forEach(course => { // コースのデータのループ処理で要素をモーダルに追加
-        $(".modal-body").append(　
+        $(".modal-body").append(
           // 状態によって適切なクラスを設定したボタンに変化を与える,三項演算子を使っている(参考：https://wa3.i-3-i.info/word11653.html)滅多に使う事はない
           `<div>
 						<span class="course-title">
 							${course.title}
 						</span>
             <button class='button ${course.joined ? "joined-button" : "join-button"}' data-id="${course._id}">
-          
+
 							${course.joined ? "Joined" : "参加"}
 						</button>
 						<div class="course-description">
@@ -114,7 +156,7 @@ let addJoinButtonListener = () => {
   });
 }; */
 
-/* 
+/*
 'courses/_coursesModal,モーダルに追加するデータ
 これがAjax関数となる
 */
@@ -127,7 +169,7 @@ let addJoinButtonListener = () => {
         $(".modal-body").append( // それぞれのコースをモーダルに加えていく(layout.js内のクラスmodal-bodyに以下を追加する)
 
         // 以下はHTMLを作成しつつ追加している
-          `<div> 
+          `<div>
 						<span class="course-title">
 							${course.title}
 						</span>
